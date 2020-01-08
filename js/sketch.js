cbaed317fd1d249f8f8beddb234c5c7c86a45284
Martin Deumens <55195056+MartinDeumens@users.noreply.github.com>
2020-01-08 15:43:00 +0100

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas (400,400);
+background(240); //groen
+
+var rood = 255;
+var vincent ="hallo";
+var blauw= 0;
+var geel = 
+console.log (vincent);
+}
+
+function draw(){
+background(0);
+  noStroke();
+
+
+
+fill(0,255,0)
+  rect(0,0,400-mouseX,400)
+}
